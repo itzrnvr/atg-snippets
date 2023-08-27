ddb2c209030a404c139014cad5da939978e9fca4
Sanskar Singh <116519896+itzrnvr@users.noreply.github.com>
2023-08-27 15:48:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 $publicKey = "-----BEGIN PUBLIC KEY-----
-(ENTER THE PUBLIC KEY HERE AFTER GETTING IT FROM REQUEST)
+(ENTER THE PUBLIC KEY HERE AFTER GETTING IT FROM REQUEST) 
 -----END PUBLIC KEY-----";
 
-$plaintext = "The quick brown fox jumps over the lazy dog";
+$data = "The quick brown fox jumps over the lazy dog"; //replace this data with the contents of the encrypted key
 $ciphertext = "";
 
-if (openssl_public_encrypt($plaintext, $ciphertext, $publicKey))
-    $encrypted = base64_encode($ciphertext); // send this back to client
+if (openssl_public_encrypt($data, $ciphertext, $publicKey))
+    $encrypted = base64_encode($ciphertext); // send this back to app in response
     print $encrypted
 ?>
